c5cf41b2745f82ff1b8e9dfc8505c6e0edb7d2ab
Winger1982 <155773608+Winger1982@users.noreply.github.com>
2026-09-13 19:44:49 +0100

    Remove CRX key prop from console

--- src/RDIConsoleMockup.tsx
+++ src/RDIConsoleMockup.tsx
@@ -3150,7 +3150,6 @@ if (!authUser) {
               >
                 <RDILiveMap
                   dataMode={dataMode}
-                  crxApiKey={crxKeyStatus === 'CONNECTED' ? crxApiKey : ''}
                   mapTheme={profile.mapTheme === 'DARK' ? 'dark' : 'light'}
                   stationProfile={{
                     operatorName: profile.operatorName,
